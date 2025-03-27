73ac077b7496ea039add083963f34960c1ff1dfd
ao627515 <ao627515@gmail.com>
2025-03-27 12:47:27 +0000

    `Fixed file name inconsistency in department routes.`

---
+++ src/controllers/department-controller.js
@@ -0,0 +1,9 @@
+// src/routes/departmentRoutes.js
+const express = require('express');
+const router = express.Router();
+const departmentController = require('../controllers/departmentController');
+
+router.post('/', departmentController.createDepartment);
+router.get('/', departmentController.getDepartments);
+
+module.exports = router;
