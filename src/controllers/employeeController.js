6fa953461c558847ce35aeb47a58d07ecb0acf92
ao627515 <ao627515@gmail.com>
2025-04-02 15:05:39 +0000

    `Added employee CRUD operations to employeeController.js and updated employeeRoutes.js to include new routes.`

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -13,11 +13,49 @@ exports.createEmployee = async (req, res) => {
   }
 };
 
-// Obtenir tous les employés avec leur département
+// Obtenir tous les employés avec leur employee
 exports.getEmployees = async (req, res) => {
   try {
     const employees = await Employee.find().populate('department');
     res.json(employees);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getEmployeesById = async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id).populate('department');;
+    if (!employee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+    res.json(employee);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Mettre à jour un employee (UPDATE)
+exports.updateEmployee = async (req, res) => {
+  try {
+    const employee = await Employee.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!employee) {
+      return res.status(404).json({ error: "Employee non trouvé" });
+    }
+    res.json(department);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Supprimer un employee (DESTROY)
+exports.deleteEmployee = async (req, res) => {
+  try {
+    const employee = await Employee.findByIdAndDelete(req.params.id);
+    if (!employee) {
+      return res.status(404).json({ error: "Employee non trouvé" });
+    }
+    res.json({ message: "Employee supprimé avec succès" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -5,5 +5,9 @@ const employeeController = require('../controllers/employeeController');
 
 router.post('/', employeeController.createEmployee);
 router.get('/', employeeController.getEmployees);
+router.get('/:id', employeeController.getEmployeesById);
+router.put('/:id', employeeController.updateEmployee);
+router.delete('/:id', employeeController.deleteEmployee);
+
 
 module.exports = router;
